afc63a686dfa99187271c41150418e1b13393af0
Matt <matthewnoyes07@gmail.com>
2021-12-03 10:34:16 +0000

    Fix sample data

--- src/main/resources/sql-data.sql
+++ src/main/resources/sql-data.sql
@@ -5,9 +5,9 @@ INSERT INTO `ims`.`items` (`name`, `price`) VALUES ('computer', 1000.00);
 INSERT INTO `ims`.`items` (`name`, `price`) VALUES ('monitor', 299.99);
 INSERT INTO `ims`.`items` (`name`, `price`) VALUES ('desk', 50.00);
 
-INSERT INTO `ims`.`orders` (`order_time`, `cust_id`) VALUES (0, 1);
-INSERT INTO `ims`.`orders` (`order_time`, `cust_id`) VALUES (1000000, 1);
-INSERT INTO `ims`.`orders` (`order_time`, `cust_id`) VALUES (2000000, 2);
+INSERT INTO `ims`.`orders` (`order_date`, `cust_id`) VALUES (NOW(), 1);
+INSERT INTO `ims`.`orders` (`order_date`, `cust_id`) VALUES (NOW(), 1);
+INSERT INTO `ims`.`orders` (`order_date`, `cust_id`) VALUES (NOW(), 2);
 
 INSERT INTO `ims`.`orderitems` (`amount`, `order_id`, 'item_id') VALUES (1, 1, 1);
 INSERT INTO `ims`.`orderitems` (`amount`, `order_id`, 'item_id') VALUES (1, 1, 2);
