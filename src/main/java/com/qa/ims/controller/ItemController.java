a1b92f11b83e1a02f1caebc517b8f7ea5df0f6c6
Africanfruit <slimbion@gmail.com>
2021-12-03 12:11:48 +0000

    added itemController

---
+++ src/main/java/com/qa/ims/controller/ItemController.java
@@ -0,0 +1,89 @@
+package com.qa.ims.controller;
+
+import com.qa.ims.persistence.dao.CustomerDAO;
+import com.qa.ims.persistence.dao.ItemDAO;
+import com.qa.ims.persistence.domain.Customer;
+import com.qa.ims.persistence.domain.Item;
+import com.qa.ims.utils.Utils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+import java.math.BigDecimal;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.List;
+
+/**
+ * Takes in item details for CRUD functionality
+ *
+ */
+public class ItemController implements CrudController<Item> {
+
+	public Connection connection;
+	public PreparedStatement statement;
+	ResultSet resultSet;
+	public static final Logger LOGGER = LogManager.getLogger();
+
+	private ItemDAO itemDAO;
+	private Utils utils;
+
+	public ItemController(ItemDAO itemDAO, Utils utils) {
+		super();
+		this.itemDAO = itemDAO;
+		this.utils = utils;
+	}
+
+	/**
+	 * Reads all items to the logger
+	 */
+	@Override
+	public List<Item> readAll() {
+		List<Item> items = itemDAO.readAll();
+		for (Item item : items) {
+			LOGGER.info(item);
+		}
+		return items;
+	}
+
+	/**
+	 * Creates an item by taking in user input
+	 */
+	@Override
+	public Item create() {
+		LOGGER.info("Please enter a name");
+		String name = utils.getString();
+		LOGGER.info("Please enter a price");
+		BigDecimal price = new BigDecimal(utils.getString());
+		return itemDAO.create(new Item(name, price));
+	}
+
+	/**
+	 * Updates an existing item by taking in user input
+	 */
+	@Override
+	public Item update() {
+		LOGGER.info("Please enter the id of the item you would like to update");
+		Long id = utils.getLong();
+		LOGGER.info("Please enter a name");
+		String name = utils.getString();
+		LOGGER.info("Please enter a price");
+		BigDecimal price = new BigDecimal(utils.getString());
+		Item item = itemDAO.update(new Item(name, price));
+		LOGGER.info("item Updated");
+		return item;
+	}
+
+	/**
+	 * Deletes an existing customer by the id of the customer
+	 * 
+	 * @return
+	 */
+	@Override
+	public int delete() {
+		LOGGER.info("Please enter the id of the customer you would like to delete");
+		Long id = utils.getLong();
+		return customerDAO.delete(id);
+	}
+
+}
